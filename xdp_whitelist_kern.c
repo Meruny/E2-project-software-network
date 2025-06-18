3e25bd8a7bd0466c505331fc03abda87c854d955
Meruny <131156767+Meruny@users.noreply.github.com>
2025-06-18 13:05:46 +0200

    Create xdp_whitelist_kern.c

---
+++ xdp_whitelist_kern.c
@@ -0,0 +1,43 @@
+// xdp_whitelist_kern.c
+
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_endian.h>
+
+// BPF map برای لیست سفید IPها
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 256);
+    __type(key, __u32);
+    __type(value, __u8);
+} whitelist_ips SEC(".maps");
+
+// تابع اصلی
+SEC("xdp")
+int xdp_whitelist(struct xdp_md *ctx) {
+    void *data = (void *)(long)ctx->data;
+    void *data_end = (void *)(long)ctx->data_end;
+
+    struct ethhdr *eth = data;
+    if ((void*)eth + sizeof(*eth) > data_end)
+        return XDP_ABORTED;
+
+    if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+        return XDP_PASS;
+
+    struct iphdr *ip = data + sizeof(*eth);
+    if ((void*)ip + sizeof(*ip) > data_end)
+        return XDP_ABORTED;
+
+    __u32 src_ip = ip->saddr;
+
+    __u8 *found = bpf_map_lookup_elem(&whitelist_ips, &src_ip);
+    if (!found)
+        return XDP_DROP;
+
+    return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
